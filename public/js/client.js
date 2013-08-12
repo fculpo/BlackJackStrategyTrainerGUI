80d8d871c1b7d1295f4e8b2388650ca3f66028f5
Fabien Culpo <fabien.culpo@gmail.com>
2013-08-12 19:16:19 +0200

    Included Stats and minor bug fixes

--- public/js/client.js
+++ public/js/client.js
@@ -9,8 +9,7 @@ bjt.socket.on('id', function(data) {
 });
 
 bjt.socket.on('updateBoard', function(data) {
-  console.log("updateBoard received");
-  console.log(data);
+  //console.log(data);
   $('#answer').empty();
   $('#dealer-hand').empty();
   $('#player-hand').empty();
@@ -19,12 +18,16 @@ bjt.socket.on('updateBoard', function(data) {
   $('#player-hand').append("<div class=\"card " + data.players[0].cards[1] + "\"></div>");
 });
 
-bjt.socket.on('actionOK', function(data) {
-  $('#answer').append("<span class=\"label label-success\">CORRECT</span>");
-});
-
-bjt.socket.on('actionKO', function(data) {
-  $('#answer').append("<span class=\"label label-danger\">WRONG. ANSWER WAS " + data["ans"] + "</span>");
+bjt.socket.on('answer', function(data) {
+  console.log(data);
+  if (data["ans"] === "OK") {
+    $('#answer').append("<span class=\"label label-success\">CORRECT</span>");
+  } else {
+    $('#answer').append("<span class=\"label label-danger\">WRONG. ANSWER WAS " + data["ans"] + "</span>");
+  }
+  $('#score-span').html(data["score"].score);
+  $('#streak-span').html(data["score"].streak);
+  $('#maxstreak-span').html(data["score"].maxStreak);
 });
 
 $('#hit-btn').click(function() {
@@ -43,10 +46,13 @@ $('#double-btn').click(function() {
   bjt.socket.emit('double');
 });
 
-$('#surrender-btn').click(function() {
-  bjt.socket.emit('surrender');
+$('#surrender-hit-btn').click(function() {
+  bjt.socket.emit('surrender-hit');
 });
 
+$('#surrender-stand-btn').click(function() {
+  bjt.socket.emit('surrender-stand');
+});
 
 console.log("client.js started, joining table");
 
