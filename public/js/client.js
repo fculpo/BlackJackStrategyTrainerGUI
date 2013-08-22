dc60239816e3b784c11698bdec0de0d8300f3227
Fabien C <fabien.culpo@gmail.com>
2013-08-22 22:27:24 +0200

    debut multiplayer

--- public/js/client.js
+++ public/js/client.js
@@ -1,60 +1,64 @@
- // Client app namespace
-var bjt = {}
+(function ($) {
+    "use strict";
 
-bjt.socket = io.connect(null, {port: 3000});
+    // Client app namespace
+    var bjt = {};
 
-bjt.socket.on('id', function(data) {
-  bjt.ClientID = data.id;
-  console.log("ClientID : " + bjt.ClientID);
-});
+    bjt.socket = io.connect(null, {port: 3000});
 
-bjt.socket.on('updateBoard', function(data) {
-  //console.log(data);
-  $('#answer').empty();
-  $('#dealer-hand').empty();
-  $('#player-hand').empty();
-  $('#dealer-hand').append("<div class=\"card " + data.dealer.cards[0] + "\"></div>");
-  $('#player-hand').append("<div class=\"card " + data.players[0].cards[0] + "\"></div>");
-  $('#player-hand').append("<div class=\"card " + data.players[0].cards[1] + "\"></div>");
-});
+    bjt.socket.on('id', function (data) {
+        bjt.ClientID = data.id;
+        console.log("ClientID : " + bjt.ClientID);
+    });
 
-bjt.socket.on('answer', function(data) {
-  console.log(data);
-  if (data["ans"] === "OK") {
-    $('#answer').append("<span class=\"label label-success\">CORRECT</span>");
-  } else {
-    $('#answer').append("<span class=\"label label-danger\">WRONG. ANSWER WAS " + data["ans"] + "</span>");
-  }
-  $('#score-span').html(data["score"].score);
-  $('#streak-span').html(data["score"].streak);
-  $('#maxstreak-span').html(data["score"].maxStreak);
-});
+    bjt.socket.on('updateBoard', function (data) {
+        //console.log(data);
+        $('#answer').empty();
+        $('#dealer-hand').empty();
+        $('#player-hand').empty();
+        $('#dealer-hand').append("<div class=\"card " + data.dealer.cards[0] + "\"></div>");
+        $('#player-hand').append("<div class=\"card " + data.players[0].cards[0] + "\"></div>");
+        $('#player-hand').append("<div class=\"card " + data.players[0].cards[1] + "\"></div>");
+    });
 
-$('#hit-btn').click(function() {
-  bjt.socket.emit('hit');
-});
+    bjt.socket.on('answer', function (data) {
+        console.log(data);
+        if (data.ans === "OK") {
+            $('#answer').append("<span class=\"label label-success\">CORRECT</span>");
+        } else {
+            $('#answer').append("<span class=\"label label-danger\">WRONG. ANSWER WAS " + data.ans + "</span>");
+        }
+        $('#score-span').html(data.score.score);
+        $('#streak-span').html(data.score.streak);
+        $('#maxstreak-span').html(data.score.maxStreak);
+    });
 
-$('#stand-btn').click(function() {
-  bjt.socket.emit('stand');
-});
+    $('#hit-btn').click(function () {
+        bjt.socket.emit('hit');
+    });
 
-$('#split-btn').click(function() {
-  bjt.socket.emit('split');
-});
+    $('#stand-btn').click(function () {
+        bjt.socket.emit('stand');
+    });
 
-$('#double-btn').click(function() {
-  bjt.socket.emit('double');
-});
+    $('#split-btn').click(function () {
+        bjt.socket.emit('split');
+    });
 
-$('#surrender-hit-btn').click(function() {
-  bjt.socket.emit('surrender-hit');
-});
+    $('#double-btn').click(function () {
+        bjt.socket.emit('double');
+    });
 
-$('#surrender-stand-btn').click(function() {
-  bjt.socket.emit('surrender-stand');
-});
+    $('#surrender-hit-btn').click(function () {
+        bjt.socket.emit('surrender-hit');
+    });
 
-console.log("client.js started, joining table");
+    $('#surrender-stand-btn').click(function () {
+        bjt.socket.emit('surrender-stand');
+    });
 
-// TEST
-bjt.socket.emit('joinTable', { pos: (Math.floor(Math.random() * 6) + 1) });
+    console.log("client.js started, joining table");
+
+    // TEST
+    bjt.socket.emit('joinTable', { pos: (Math.floor(Math.random() * 6) + 1) });
+})(jQuery);
