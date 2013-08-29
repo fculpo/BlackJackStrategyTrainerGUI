78a7b5080d639782f2166888518827fd27e68915
Fabien C <fabien.culpo@gmail.com>
2013-08-29 19:02:28 +0200

    auto reshuffle on penetration rate
    suppressed Deck.js (useless)

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
   debug: true,
 
-  howManyDecks: 6,
-  howManyPlayersMax: 6, 
+  howManyDecks: 1,
+  howManyPlayersMax: 6,
   startingStack: 500,
+  penetrationRate: 0.75,
 
   dev: {
     port: process.env.PORT || 3000,
