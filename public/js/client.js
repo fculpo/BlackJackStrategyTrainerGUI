11e59ebfa10bf9a1ee4f2f0a6b5672e7f0ce29d4
Fabien C <fabien.culpo@gmail.com>
2013-08-31 23:41:18 +0200

    Answer feedback improvements
    Blink correct answer

--- public/js/client.js
+++ public/js/client.js
@@ -3,20 +3,56 @@
 // Client app namespace
 var bjt = {};
 
+bjt.buttonsToActionMap = {
+  "SURRENDER": $('#surrender-btn'),
+  "HIT": $('#hit-btn'),
+  "STAND": $('#stand-btn'),
+  "SPLIT": $('#split-btn'),
+  "DOUBLE": $('#double-btn')
+}
+
+bjt.blinkButton = function (button) {
+
+    button.removeClass().addClass("btn btn-success");
+    setTimeout(function() {
+      button.removeClass().addClass("btn btn-default");
+      setTimeout(function() {
+        button.removeClass().addClass("btn btn-success");
+        setTimeout(function() {
+          button.removeClass().addClass("btn btn-default");
+          setTimeout(function() {
+            button.removeClass().addClass("btn btn-success");
+          }, 150);
+        }, 150);
+      }, 150);
+    }, 150);
+
+}
+
 bjt.disableButtons = function() {
   $('#hit-btn').prop('disabled', true);
+  $('#hit-btn').removeClass().addClass("btn btn-default");
   $('#stand-btn').prop('disabled', true);
+  $('#stand-btn').removeClass().addClass("btn btn-default");
   $('#split-btn').prop('disabled', true);
+  $('#split-btn').removeClass().addClass("btn btn-default");
   $('#double-btn').prop('disabled', true);
+  $('#double-btn').removeClass().addClass("btn btn-default");
   $('#surrender-btn').prop('disabled', true);
+  $('#surrender-btn').removeClass().addClass("btn btn-default");
 }
 
 bjt.enableButtons = function() {
   $('#hit-btn').prop('disabled', false);
+  $('#hit-btn').removeClass().addClass("btn btn-primary");
   $('#stand-btn').prop('disabled', false);
+  $('#stand-btn').removeClass().addClass("btn btn-danger");
   $('#split-btn').prop('disabled', false);
+  $('#split-btn').removeClass().addClass("btn btn-warning");
   $('#double-btn').prop('disabled', false);
+  $('#double-btn').removeClass().addClass("btn btn-info");
   $('#surrender-btn').prop('disabled', false);
+  $('#surrender-btn').removeClass().addClass("btn btn-default");
 }
 
 bjt.socket = io.connect(window.location.hostname, {'sync disconnect on unload' : true});
@@ -44,11 +80,13 @@ bjt.socket.on('updateBoard', function (data) {
 
 bjt.socket.on('answer', function (data) {
     console.log(data);
-    if (data.ans === "OK") {
+    if (data.res === "OK") {
         $('#answer').append("<span class=\"label label-success\">CORRECT</span>");
     } else {
         $('#answer').append("<span class=\"label label-danger\">WRONG. ANSWER WAS " + data.ans + "</span>");
     }
+    //bjt.buttonsToActionMap[data.ans].removeClass().addClass("btn btn-success");
+    bjt.blinkButton(bjt.buttonsToActionMap[data.ans]);
     $('#score-span').html(data.score.score);
     $('#streak-span').html(data.score.streak);
     $('#maxstreak-span').html(data.score.maxStreak);
